528905e4e58d94483005855109bb4a3ac45c30e5
Deleo Anangfac <tendongmohdeleoanangfac@gmail.com>
2023-07-29 10:24:08 +0100

    FRAMEWORK

--- FRAMEWORK/DJANGO/RENT/rent/settings.py
+++ FRAMEWORK/DJANGO/RENT/rent/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u9ixo5_xjjh=b(9#74t7w37)o3wt^)$0o$kqa-ua)^u^r%)13h'
+SECRET_KEY = 'YOUR_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
